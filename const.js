5cf65bc4fe649b02bcafd6f468baf6d960315a29
Daniel Lodewikus Meyer <meyerdln@icloud.com>
2019-02-18 11:54:04 +0200

    Add files via upload

---
+++ const.js
@@ -0,0 +1,11 @@
+const person = {
+    age: 27
+}
+
+// person = {}
+person.age = 28
+
+// isRaining = false
+
+console.log(person)
+
--- number-methods.js
+++ number-methods.js
@@ -5,3 +5,25 @@ console.log(num.toFixed(1))
 console.log(Math.round(num))
 console.log(Math.floor(num))
 console.log(Math.ceil(num))
+
+let min = 0
+let max = 1
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+// 10 - 20
+console.log(randomNum)
+
+// Challenge area 
+
+// 1-5 - true if correct  - false if not correct
+
+let makeGuess = function (guess) {
+    let min = 1
+    let max = 5
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+
+    return guess === randomNum
+}
+
+console.log(makeGuess(1))
+
+
