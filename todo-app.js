bef800748fde22cd8935af3aa24978abcb88e0e5
Daniel Lodewikus Meyer <meyerdln@icloud.com>
2019-02-25 12:11:15 +0200

    Add files via upload

---
+++ todo-app.js
@@ -0,0 +1,9 @@
+
+
+const paragraphs = document.querySelectorAll('p')
+
+paragraphs.forEach(function (paragraph) {
+    if(paragraph.textContent.includes('the')) {
+        paragraph.remove()
+    }
+})
